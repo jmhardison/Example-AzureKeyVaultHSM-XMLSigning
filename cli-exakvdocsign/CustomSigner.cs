2f2b82b34aa6ede8b1a93f94d307509c326fc162
Jonathan Hardison <jmh@jonathanhardison.com>
2018-03-08 09:30:42 -0600

    readme updates

--- cli-exakvdocsign/CustomSigner.cs
+++ cli-exakvdocsign/CustomSigner.cs
@@ -17,7 +17,7 @@ namespace cli_exakvdocsign
     {
         public byte[] Sign(byte[] data)
         {
-            return cli_exakvdocsign.Program.keyVault.SignAsync(cli_exakvdocsign.Program.KeyVaultAddress, cli_exakvdocsign.Program.KeyName, cli_exakvdocsign.Program.KeyVersion, Microsoft.Azure.KeyVault.WebKey.JsonWebKeySignatureAlgorithm.RS256, data).Result.Result;
+            return cli_exakvdocsign.Program.keyVault.SignAsync(cli_exakvdocsign.Program.KeyVaultAddress, cli_exakvdocsign.Program.publicCertBundle.KeyIdentifier.Name, cli_exakvdocsign.Program.publicCertBundle.KeyIdentifier.Version, Microsoft.Azure.KeyVault.WebKey.JsonWebKeySignatureAlgorithm.RS256, data).Result.Result;
         }
     }
 }
--- cli-exakvdocsign/Program.cs
+++ cli-exakvdocsign/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+    Example Azure KeyVault HSM Protected Key XML Signing
+ */
+
+using System;
 using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
@@ -17,41 +21,56 @@ namespace cli_exakvdocsign
 {
 class Program
     {
+        // AAD Application ID - Create a new application registration and grant it appropriate rights to the KeyVault.
         private static string AADClientID;
+        // AAD Application Secret/Key - Create a "password key" that lives for the desired validity period before expiring.
         private static string AADClientSecret;
-
+        // KeyVault Address - Full address of deployed KeyVault. 
         internal static string KeyVaultAddress;
-
-        internal static string KeyName;
+        //certificate name  - Provided in creation of cert, also visible in key identifier. (*.vault.azure.net/keys/<certnamehere/*)
+        internal static string CertName;
+        //file name and path of original xml document.
         private static string XMLFileName;
+        //file name and path of where to save the signed xml document.
         private static string XMLFileOutputName;
 
+        //instantiated KeyVault client used through the example.
         internal static KeyVaultClient keyVault;
+        //retrieved keybundle (private key). Note, the actual private key is never returned by Azure KeyVault HSM.
         internal static KeyBundle secretKeyBundle;
+        //retrieved public certificate bundle. This will also hold a reference the the appropriate private key information.
         internal static CertificateBundle publicCertBundle;
-        internal static string KeyVersion;
+        
+        //holds file name of signed xml to check. When populated, no other actions are ran.
         private static string ValidateFile = null;
 
        
-
+        
         static void Main(string[] args)
         {
             try{
+                //run through the arguments and break them into variables.
                 ProcessArgs(args);
 
+                //if user is requesting validaiton of a file, only run verifydoc and exit.
                 if(ValidateFile != null)
                 {
                     VerifyDoc();
                 }
                 else{
-                
+                    
+                    //create KeyVault Client and authenticate with AAD. (GetAccessToken method.)
                     keyVault = new KeyVaultClient(
                         new KeyVaultClient.AuthenticationCallback(GetAccessToken),
                         new HttpClient());
                     
-                    publicCertBundle = keyVault.GetCertificateAsync(KeyVaultAddress, KeyName).Result;
+                    //get cert bundle from supplied certificate name.
+                    publicCertBundle = keyVault.GetCertificateAsync(KeyVaultAddress, CertName).Result;
+                    //get secret (private key) bundle from information in public cert bundle pointing to current key and version associated to cert.
                     secretKeyBundle = keyVault.GetKeyAsync(publicCertBundle.KeyIdentifier.Vault, publicCertBundle.KeyIdentifier.Name).Result;
+                    
 
+                    //sign the doc yo!
                     SignDoc();
                 }
             }
@@ -59,6 +78,66 @@ class Program
                 Console.Write("\n\n Error Caught: \n\n" + e.Message);
             }
         }
+
+        public static async Task<string> GetAccessToken(string authority, string resource, string scope)  
+        {  
+            ClientCredential clientCredential = new ClientCredential(AADClientID, AADClientSecret);  
+        
+            var context = new AuthenticationContext(authority, TokenCache.DefaultShared);  
+            var result = await context.AcquireTokenAsync(resource, clientCredential);  
+        
+            return result.AccessToken;  
+        }  
+
+        private static void ProcessArgs(string[] args){
+            if(args.Length > 0){
+                for(int i =0; i < args.Length; i++){
+                    if(args[i].StartsWith("-validateonlyfile")){
+                        ValidateFile = args[i+1];
+                        break;
+                    }
+                    else if(args[i].StartsWith("-aadclientid")){
+                        AADClientID = args[i+1];
+                    }
+                    else if(args[i].StartsWith("-aadclientsecret")){
+                        AADClientSecret = args[i+1];
+                    }
+                    else if(args[i].StartsWith("-keyvaulturl")){
+                        KeyVaultAddress = args[i+1];
+                    }
+                    else if(args[i].StartsWith("-certname")){
+                        CertName = args[i+1];
+                    }
+                    else if(args[i].StartsWith("-xmlfiletosign")){
+                        XMLFileName = args[i+1];
+                    }
+                    else if(args[i].StartsWith("-xmlfiletosave")){
+                        XMLFileOutputName = args[i+1];
+                    }
+                }
+            }
+            else{
+                Console.WriteLine("Arguments are required. \n");
+                PrintHelp();
+            }
+        }
+
+        private static void PrintHelp(){
+            Console.WriteLine("Example-AzureKeyVaultHMS Document Signing\n");
+            Console.WriteLine("===============================================================================\n");
+            Console.WriteLine("-aadclientid : Provide the Azure AD Client ID with access to KeyVault.\n");
+            Console.WriteLine("-aadclientsecret : Provide the Client Secret for AAD Client ID.\n");
+            Console.WriteLine("-keyvaulturl : URL of KeyVault, such as https://vaultname.vault.azure.net/ \n");
+            Console.WriteLine("-keyname : name of stored key in vault.\n");
+            Console.WriteLine("-xmlfiletosign : path and filename of file to sign.\n");
+            Console.WriteLine("-xmlfiletosave : path and filename of file to save to.\n");
+            Console.WriteLine("-validateonlyfile : used to only validate the specified file.\n");
+            Console.WriteLine("-------------------------------------------------------------------------------\n");
+            Console.WriteLine("Example: cli-exakvdocsign -aadclientid 123 -aadclientsecret 123= -keyvaulturl https://dog.vault.azure.net/ -keyname bob -filetosign filename.xml -xmlfiletosave savetofilename.xml\n");
+            Console.WriteLine("===============================================================================\n");
+            Environment.Exit(-1);
+        }
+
 
         public static void SignDoc(){
             try{
@@ -89,69 +168,6 @@ class Program
                 // Sign the XML document. 
                 Console.WriteLine("The file validation results in: " + Verify(xmlDoc));
         }
-
-        public static async Task<string> GetAccessToken(string authority, string resource, string scope)  
-        {  
-            ClientCredential clientCredential = new ClientCredential(AADClientID, AADClientSecret);  
-        
-            var context = new AuthenticationContext(authority, TokenCache.DefaultShared);  
-            var result = await context.AcquireTokenAsync(resource, clientCredential);  
-        
-            return result.AccessToken;  
-        }  
-
-        private static void ProcessArgs(string[] args){
-            if(args.Length > 0){
-                for(int i =0; i < args.Length; i++){
-                    if(args[i].StartsWith("-validateonlyfile")){
-                        ValidateFile = args[i+1];
-                        break;
-                    }
-                    else if(args[i].StartsWith("-aadclientid")){
-                        AADClientID = args[i+1];
-                    }
-                    else if(args[i].StartsWith("-aadclientsecret")){
-                        AADClientSecret = args[i+1];
-                    }
-                    else if(args[i].StartsWith("-keyvaulturl")){
-                        KeyVaultAddress = args[i+1];
-                    }
-                    else if(args[i].StartsWith("-keyname")){
-                        KeyName = args[i+1];
-                    }
-                    else if(args[i].StartsWith("-xmlfiletosign")){
-                        XMLFileName = args[i+1];
-                    }
-                    else if(args[i].StartsWith("-xmlfiletosave")){
-                        XMLFileOutputName = args[i+1];
-                    }
-                    else if(args[i].StartsWith("-keyversion")){
-                        KeyVersion = args[i+1];
-                    }
-                }
-            }
-            else{
-                Console.WriteLine("Arguments are required. \n");
-                PrintHelp();
-            }
-        }
-
-        private static void PrintHelp(){
-            Console.WriteLine("Example-AzureKeyVaultHMS Document Signing\n");
-            Console.WriteLine("===============================================================================\n");
-            Console.WriteLine("-aadclientid : Provide the Azure AD Client ID with access to KeyVault.\n");
-            Console.WriteLine("-aadclientsecret : Provide the Client Secret for AAD Client ID.\n");
-            Console.WriteLine("-keyvaulturl : URL of KeyVault, such as https://vaultname.vault.azure.net/ \n");
-            Console.WriteLine("-keyname : name of stored key in vault.\n");
-            Console.WriteLine("-xmlfiletosign : path and filename of file to sign.\n");
-            Console.WriteLine("-xmlfiletosave : path and filename of file to save to.\n");
-            Console.WriteLine("-validateonlyfile : used to only validate the specified file.\n");
-            Console.WriteLine("-------------------------------------------------------------------------------\n");
-            Console.WriteLine("Example: cli-exakvdocsign -aadclientid 123 -aadclientsecret 123= -keyvaulturl https://dog.vault.azure.net/ -keyname bob -filetosign filename.txt\n");
-            Console.WriteLine("===============================================================================\n");
-            Environment.Exit(-1);
-        }
-
 
         //sourced https://docs.microsoft.com/en-us/dotnet/standard/security/how-to-sign-xml-documents-with-digital-signatures
         // Sign an XML file. 
